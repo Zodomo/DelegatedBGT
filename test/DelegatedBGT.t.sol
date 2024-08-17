8e858e227e5d2b8961699faf1b181af56c46ec34
Zodomo <zodomo@proton.me>
2024-08-16 20:54:03 -0500

    fixed foundry issues with update, queue/cancel work, activate throws NotActivated from unverified contract

--- test/DelegatedBGT.t.sol
+++ test/DelegatedBGT.t.sol
@@ -12,6 +12,7 @@ contract DelegatedBGTTest is Test {
     address delegator = 0x9a085B397b12A2ff95759fE3a26518f934a03123;
     address validator = 0x2D764DFeaAc00390c69985631aAA7Cc3fcfaFAfF;
     address delegate;
+    uint128 unboosted;
 
     modifier prank(address addr) {
         vm.startPrank(addr);
@@ -28,12 +29,12 @@ contract DelegatedBGTTest is Test {
 
     // Overwrite BGT contract with our version and initialize `registry`
     function prepare() public {
-        vm.roll(2995082);
-        // NOTE: Etch currently seems to possibly overwrite storage (error maybe?), is there a way to only upgrade code?
+        vm.roll(2995083);
         bgt = new BGT();
         bgt.initialize(owner);
         vm.etch(0xbDa130737BDd9618301681329bF2e46A016ff9Ad, address(bgt).code);
         bgt = BGT(0xbDa130737BDd9618301681329bF2e46A016ff9Ad);
+        unboosted = uint128(bgt.unboostedBalanceOf(delegator));
         registry = DelegateRegistry(0x00000000000000447e69651d841bD8D104Bed493);
     }
 
@@ -45,44 +46,42 @@ contract DelegatedBGTTest is Test {
         bgt.mint(delegator, 1 ether);
     }*/
 
-    function queue() public prank(delegate) {
-        bgt.queueBoost(delegator, validator, 1);
+    function queue(uint128 amount) public prank(delegate) {
+        bgt.queueBoost(delegator, validator, amount);
     }
 
-    function cancel() public prank(delegate) {
-        bgt.cancelBoost(delegator, validator, 1);
+    function cancel(uint128 amount) public prank(delegate) {
+        bgt.cancelBoost(delegator, validator, amount);
     }
 
     function activate() public prank(delegate) {
         bgt.activateBoost(delegator, validator);
     }
 
-    function drop() public prank(delegate) {
-        bgt.dropBoost(delegator, validator, 1);
+    function drop(uint128 amount) public prank(delegate) {
+        bgt.dropBoost(delegator, validator, amount);
     }
 
     function testQueue() public {
-        console2.log(block.number);
-        // `delegator` should have 64900877015889774 wei of unboosted balance at this block number (source: Beratrail)
-        // Why does this return 0? Is the etch overwriting the storage slots? Is RPC incompatible with foundry?
-        bgt.unboostedBalanceOf(delegator);
-        queue();
+        queue(unboosted);
     }
 
-    /*function testCancel() public {
-        queue();
-        cancel();
+    function testCancel() public {
+        queue(unboosted);
+        cancel(unboosted);
     }
 
+    // TODO: Identify why activateBoost throws `EvmError: NotActivated`
     function testActivate() public {
-        queue();
-        vm.roll(block.number + 8192);
+        queue(unboosted);
+        vm.roll(3003275);
         activate();
     }
 
-    function testDrop() public {
-        queue();
+    /*function testDrop() public {
+        queue(unboosted);
+        vm.roll(3003275);
         activate();
-        drop();
+        drop(unboosted);
     }*/
 }
