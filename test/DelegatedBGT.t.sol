6b06564a265d6a01f8f5cf785f73880c70242a77
Zodomo <zodomo@proton.me>
2024-08-16 19:44:58 -0500

    tried to mock a valid, previous action. seems etch overwrites storage.

--- test/DelegatedBGT.t.sol
+++ test/DelegatedBGT.t.sol
@@ -8,10 +8,10 @@ import {DelegateRegistry} from "delegate-registry/src/DelegateRegistry.sol";
 contract DelegatedBGTTest is Test {
     BGT bgt;
     DelegateRegistry registry;
-    address owner;
-    address delegator;
+    //address owner = 0x8a73D1380345942F1cb32541F1b19C40D8e6C94B;
+    address delegator = 0x9a085B397b12A2ff95759fE3a26518f934a03123;
+    address validator = 0x2D764DFeaAc00390c69985631aAA7Cc3fcfaFAfF;
     address delegate;
-    address validator;
 
     modifier prank(address addr) {
         vm.startPrank(addr);
@@ -19,10 +19,17 @@ contract DelegatedBGTTest is Test {
         vm.stopPrank();
     }
 
+    function setUp() public {        
+        delegate = makeAddr("delegate");
+        vm.createSelectFork("testnet");
+        prepare();
+        delegation();
+    }
+
     // Overwrite BGT contract with our version and initialize `registry`
     function prepare() public {
+        // NOTE: Etch currently seems to overwrite storage too, is there a way to only upgrade code?
         bgt = new BGT();
-        bgt.initialize(owner);
         vm.etch(0xbDa130737BDd9618301681329bF2e46A016ff9Ad, address(bgt).code);
         bgt = BGT(0xbDa130737BDd9618301681329bF2e46A016ff9Ad);
         registry = DelegateRegistry(0x00000000000000447e69651d841bD8D104Bed493);
@@ -32,16 +39,16 @@ contract DelegatedBGTTest is Test {
         registry.delegateAll(delegate, bytes32(''), true);
     }
 
-    function mint() public prank(bgt.minter()) {
+    /*function mint() public prank(bgt.minter()) {
         bgt.mint(delegator, 1 ether);
-    }
+    }*/
 
     function queue() public prank(delegate) {
-        bgt.queueBoost(delegator, validator, 1 ether);
+        bgt.queueBoost(delegator, validator, 64900877015889774);
     }
 
     function cancel() public prank(delegate) {
-        bgt.cancelBoost(delegator, validator, 1 ether);
+        bgt.cancelBoost(delegator, validator, 64900877015889774);
     }
 
     function activate() public prank(delegate) {
@@ -49,25 +56,15 @@ contract DelegatedBGTTest is Test {
     }
 
     function drop() public prank(delegate) {
-        bgt.dropBoost(delegator, validator, 1 ether);
-    }
-
-    function setUp() public {
-        owner = makeAddr("owner");
-        delegator = makeAddr("delegator");
-        delegate = makeAddr("delegate");
-        validator = makeAddr("validator");
-        vm.createSelectFork("testnet");
-        prepare();
-        delegation();
-        mint();
+        bgt.dropBoost(delegator, validator, 64900877015889774);
     }
 
     function testQueue() public {
+        vm.roll(2995082);
         queue();
     }
 
-    function testCancel() public {
+    /*function testCancel() public {
         queue();
         cancel();
     }
@@ -82,5 +79,5 @@ contract DelegatedBGTTest is Test {
         queue();
         activate();
         drop();
-    }
+    }*/
 }
